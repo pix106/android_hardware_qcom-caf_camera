c98fc5a6fc14e42c8ce1c112d5c9a398d74b4dbc
Sabar <31942277+sabarop@users.noreply.github.com>
2023-09-17 01:02:47 +0700

    camera : remove IS_TYPE_VENDOR_EIS
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- QCamera2/HAL3/QCamera3HWI.cpp
+++ QCamera2/HAL3/QCamera3HWI.cpp
@@ -3160,9 +3160,11 @@ int QCamera3HardwareInterface::configureStreamsPerfLocked(
                             channel->setUBWCEnabled(true);
                         }
                         newStream->max_buffers = channel->getNumBuffers();
+#if 0
                         if (isTypeVideo == IS_TYPE_VENDOR_EIS) {
                             newStream->max_buffers = MAX_VIDEO_VENDOR_EIS_BUFFERS;
                         }
+#endif
                         newStream->priv = channel;
                     }
                     break;
@@ -8327,6 +8329,7 @@ no_error:
                     return BAD_VALUE;
                 }
             }
+#if 0
             if ((isTypePreview == IS_TYPE_VENDOR_EIS) &&
                (((1U << CAM_STREAM_TYPE_PREVIEW) == channel->getStreamTypeMask()))) {
                 //Trigger Perf Flush event to back-end
@@ -8337,6 +8340,7 @@ no_error:
                     return BAD_VALUE;
                 }
             }
+#endif
         }
     }
 
