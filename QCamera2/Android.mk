0122b4808132c8d3a5e827001f874349f73150a1
pix106 <sbordenave@gmail.com>
2023-09-16 13:26:31 +0200

    camera: fix include path

--- QCamera2/Android.mk
+++ QCamera2/Android.mk
@@ -112,8 +112,8 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/stack/mm-camera-interface/inc \
         $(LOCAL_PATH)/util \
         $(LOCAL_PATH)/HAL3 \
-        hardware/qcom/media/libstagefrighthw \
-        hardware/qcom/media/mm-core/inc \
+        hardware/qcom-caf/sdm660/media/libstagefrighthw \
+        hardware/qcom-caf/sdm660/media/mm-core/inc \
         $(TARGET_OUT_HEADERS)/mm-camera-lib/cp/prebuilt
 
 ifneq ($(TARGET_KERNEL_VERSION),$(filter $(TARGET_KERNEL_VERSION),3.18 4.4 4.9))
